ebe5ef54518dbb5a8520ec90b838992756d906b4
mamedians2 <mammameedian@gmail.com>
2021-05-09 18:27:48 +0900

    完成。

--- app.js
+++ app.js
@@ -33,27 +33,30 @@ const quiz = [
 
 const q = document.getElementById('js-question');
 const $btn = document.getElementsByTagName('button');
-let i = 0;
 let quizIndex = 0;
+let score = 0;
 
 //問題文と、選択肢を表示
 const setupQuiz = () => {
   
   q.textContent = quiz[quizIndex].question;
-
+  let i = 0;
   while(i < $btn.length){
     $btn[i].textContent = quiz[quizIndex].answers[i];
     i++;
   }
-
 }
 
 setupQuiz();
 
+//正誤判定の関数
+// 回答したら得点
+//全問回答したら終了アラート
 const clickHandler = (e) => {
 
   if(quiz[quizIndex].correnct === e.target.textContent){
     window.alert('正解！');
+    score++;
   }else {
     window.alert('不正解。');
   }
@@ -63,13 +66,11 @@ const clickHandler = (e) => {
   if(quizIndex < quiz.length){
     setupQuiz();
   }else {
-    window.alert('回答終了！');
+    window.alert('回答終了！' + score + '\/' + quiz.length + '問正解しました！');
   }
-
 }
 
-//ボタンを押したら正誤判定
-
+// ボタンを押したら上記の関数を走らせる
 let btnIndex = 0;
 while( btnIndex < $btn.length ){
 
